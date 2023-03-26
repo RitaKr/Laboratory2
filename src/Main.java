ae4b9f959a770718fa8cd08a66f694804063d4cd
RitaKr <ritokra@gmail.com>
2023-03-27 00:58:00 +0300

    commented tests

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
         //testing
 
-        factory.addProductsGroup("group3",  "this is product1");
-        factory.addProduct("group3", "product4", "this is product1", "producer", 5, 100);
-        System.out.println(factory);
-        factory.showAllProductsCost();
-        factory.showAllProductsByGroup("group4");
-        factory.showAllProductsCostByGroup("group4");
+//        factory.addProductsGroup("group3",  "this is product1");
+//        factory.addProduct("group3", "product4", "this is product1", "producer", 5, 100);
+//        System.out.println(factory);
+//        factory.showAllProductsCost();
+//        factory.showAllProductsByGroup("group4");
+//        factory.showAllProductsCostByGroup("group4");
 
     }
 }
